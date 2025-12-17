44dc81dfed68f5f519dbf4ed93678f3616cee6f5
Milena <miimillena28@gmail.com>
2025-12-16 21:53:36 -0300

    Add files via upload

---
+++ 08. conversao.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+	var a int = 20
+	var b string = "Nome"
+
+	func main(){
+		var c float64 = float64(a)
+		fmt.Println("O valor de C é:",c,"e o valor de b é:", b)
+	}
