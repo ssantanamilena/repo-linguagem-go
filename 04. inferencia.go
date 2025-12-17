5f2372226d580263d9b715b5719bb11492553e91
Milena <miimillena28@gmail.com>
2025-12-16 21:25:22 -0300

    Add files via upload

---
+++ 04. inferencia.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	var nome = "Milena"
+	var idade = 29
+	var versao = 2.2
+	fmt.Println("Meu nome é", nome, "e minha idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+}
